82f5a7a6b84b3954c6426247ade45761273d5722
Romeo Asinari <ralonsoad@gmail.com>
2023-06-29 11:54:28 +0200

    First_component

---
+++ Reactapp/src/Message.tsx
@@ -0,0 +1,13 @@
+
+
+// Pascalcasing  Capitalice the first letter of every word
+
+
+
+function Message() {
+//JSX  javascript XML declaration  
+return <h1> hello world</h1>;
+}
+
+// to use the component, we need to export it
+export default Message;
